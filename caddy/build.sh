cff008c27129ead71aa22d2af7b1f66ae609fedb
joker <joker@ci>
2018-09-22 20:02:22 +0200

    Fix caddy makefile

--- caddy/build.sh
+++ caddy/build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ -d $GOPATH/src/github.com/mholt/caddy/caddy/caddymain ]; then
+    echo 'Reverting last build state...'
+    cd $GOPATH/src/github.com/mholt/caddy/caddy/caddymain && \
+    git checkout run.go || true
+fi
+
 # Fetch Caddy server source code
 echo 'Fetching source code...'
 go get github.com/mholt/caddy/caddy
@@ -12,12 +18,12 @@ cd $GOPATH/src/github.com/mholt/caddy/caddy/caddymain && \
 
 # Fill list of plugins
 echo 'Adding plugins...'
-awk '/This is where other plugins get plugged in/ { print; print "//CADDY_PLUGIN"; next }1' run.go > run.go.temp && \
+awk '/This is where other plugins get plugged in/ { print; print "//CADDY_PLUGINS"; next }1' run.go > run.go.temp && \
     mv run.go.temp run.go
 while read p; do
     awk -v plugin="$p" \
-        '/\/\/CADDY_PLUGIN/ { print; print "_ \""plugin"\" //CADDY_PLUGIN"; next }1' \
-        run.go > run.go.temp && \
+        'FNR==NR{ if (/\/\/CADDY_PLUGINS/) p=NR; next} 1; FNR==p{ print "_ \""plugin"\"" }' \
+        run.go run.go > run.go.temp && \
     mv run.go.temp run.go
     go get $p
     echo "Added plugin $p"
@@ -29,5 +35,5 @@ cd $GOPATH/src/github.com/mholt/caddy/caddy && \
     go run build.go
 
 # Copy build result to mounted volume
-cp caddy /tmp
+cp caddy /tmp/caddy_build
 echo 'Done.'
