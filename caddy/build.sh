c3a2850627e5fcc1b9a1b7d00c2907b7c84575ff
x <x@y.z>
2019-04-26 14:33:56 +0000

    Adapt to caddy==1.0.0

--- caddy/build.sh
+++ caddy/build.sh
@@ -1,43 +1,36 @@
 #!/usr/bin/env bash
-CADDY_VERSION=v0.11.5
-
-# Fetch Caddy server source code
-echo 'Fetching source code...'
-go get github.com/mholt/caddy/caddy
-go get github.com/caddyserver/builds
-
-if [ -d $GOPATH/src/github.com/mholt/caddy/caddy/caddymain ]; then
-    echo 'Reverting last build state...'
-    cd $GOPATH/src/github.com/mholt/caddy/caddy/caddymain && \
-    git checkout run.go || true
-    echo "Checking out caddy@$CADDY_VERSION..."
-    cd $GOPATH/src/github.com/mholt/caddy/caddy && \
-    git fetch --tags --force && \
-    git checkout $CADDY_VERSION
-fi
-
-# Turn off telemetry by modifying source code
-echo 'Turning off telemetry...'
-cd $GOPATH/src/github.com/mholt/caddy/caddy/caddymain && \
-    sed -i 's/EnableTelemetry = true/EnableTelemetry = false/g' run.go
+export GO111MODULE=on
+export CADDY_VERSION=v1.0.0
+SELFDIR=$(dirname $0)
+CWD=/tmp/build
+mkdir -p $CWD
+# Prepare main module source code file
+cd $CWD
+cp $SELFDIR/main.go.template main.go
 
 # Fill list of plugins
 echo 'Adding plugins...'
-awk '/This is where other plugins get plugged in/ { print; print "//CADDY_PLUGINS"; next }1' run.go > run.go.temp && \
-    mv run.go.temp run.go
 while read p; do
     awk -v plugin="$p" \
-        'FNR==NR{ if (/\/\/CADDY_PLUGINS/) p=NR; next} 1; FNR==p{ print "_ \""plugin"\"" }' \
-        run.go run.go > run.go.temp && \
-    mv run.go.temp run.go
-    go get $p
+        'FNR==NR{ if (/\/\/CADDY_PLUGINS/) p=NR; next} 1; FNR==p{ print "	_ \""plugin"\"" }' \
+        main.go main.go > main.go.temp && \
+    mv main.go.temp main.go
     echo "Added plugin $p"
-done <$(dirname $0)/plugins.txt
+done <$SELFDIR/plugins.txt
+# Remove the indicator line
+sed -i '/\/\/CADDY_PLUGINS/d' main.go
+cat $CWD/main.go
 
 # Build Caddy server
+echo 'Init modules...'
+[[ -f "go.mod" ]] && rm go.mod
+[[ -f "go.sum" ]] && rm go.sum
+go mod init caddy
 echo 'Building...'
-cd $GOPATH/src/github.com/mholt/caddy/caddy && \
-    go run build.go
+go build
+
+# Remove used main module source code file
+rm main.go
 
 # Copy build result to mounted volume
 cp caddy /tmp/caddy_build
